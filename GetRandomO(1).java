570b1b2887ab492f1253ea5cd0b043832c9e71eb
jsabzt <email@example.com>
2024-11-22 00:47:18 -0500

    Insert Delete Get Random O(1)

---
+++ GetRandomO(1).java
@@ -0,0 +1,44 @@
+class RandomizedSet {
+    List<Integer> list;
+    Map<Integer, Integer> map;
+
+    public RandomizedSet() {
+        this.list = new ArrayList<>();
+        this.map = new HashMap<>();
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        list.add(val);
+        map.put(val, list.size() - 1); // Map value to its index in the list
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        // Get the index of the value to be removed
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+
+        // Swap the value to be removed with the last element
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+
+        // Remove the last element
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+    
+    public int getRandom() {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+}
